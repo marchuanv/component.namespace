b17487c0fc2a358bf61e38475405707f0ff10e5e
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-23 02:57:52 +0200

    refactor

--- specs/namespace-spec.mjs
+++ specs/namespace-spec.mjs
@@ -1,31 +1,50 @@
 import { Namespace } from '../registry.mjs';
+class NamespaceA extends Namespace {
+    constructor(segments = []) {
+        segments.push('SegmentA');
+        return super(segments);
+    }
+}
+class NamespaceB extends NamespaceA {
+    constructor(segments = []) {
+        segments.push('SegmentB');
+        return super(segments);
+    }
+}
+class InvalidNamespace extends NamespaceB {
+    constructor() {
+        return super(['$##@.%#$%']);
+    }
+}
 describe('when creating a valid namespace', () => {
     it('should have equality between two of the same namespaces', () => {
-        const id = new Namespace('segment1');
-        const id2 = new Namespace('segment1');
+        const id = new NamespaceA();
+        const id2 = new NamespaceA();
         expect(id).toBeDefined();
         expect(id).not.toBeNull();
         expect(id2).toBeDefined();
         expect(id2).not.toBeNull();
         expect(id).toBe(id2);
         expect(id).toEqual(id2);
+        expect(id.value).toBe(id2.value);
     });
     it('should not have equality between two different namespaces', () => {
-        const id = new Namespace('segment1.segment1');
-        const id2 = new Namespace('segment1.segment2');
+        const id = new NamespaceB();
+        const id2 = new NamespaceA();
         expect(id).toBeDefined();
         expect(id).not.toBeNull();
         expect(id2).toBeDefined();
         expect(id2).not.toBeNull();
         expect(id).not.toBe(id2);
-        expect(id).toEqual(id2);
+        expect(id).not.toEqual(id2);
+        expect(id.value).not.toBe(id2.value);
     });
 });
 describe('when creating an invalid namespace given a namespace of "part.$##@.%#$%"', () => {
     let error = null;
     beforeAll(() => {
         try {
-            new Namespace('part.$##@.%#$%');
+            new InvalidNamespace();
         } catch (err) {
             error = err;
         }
@@ -33,6 +52,6 @@ describe('when creating an invalid namespace given a namespace of "part.$##@.%#$
     it('should raise an error', () => {
         expect(error).toBeDefined();
         expect(error).not.toBeNull();
-        expect(error.message).toBe('The namespace argument does not adhering to typical naming conventions: requires optional "@" prefixes and multiple segments separated by dots.')
+        expect(error.message).toBe('Invalid namespace format: The namespace argument must adhere to typical naming conventions, which include optional "@" prefixes and multiple segments separated by dots. Example: "@example.namespace.segment"')
     });
 });
